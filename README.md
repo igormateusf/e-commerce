e444eba157b9fed3d60279e7aec2db45975aecb1
Igor França <igorfrancadiniz@gmail.com>
2023-08-09 16:08:29 -0300

    Adição das pastas de views

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Projeto E-commerce
 
-Projeto desenvolvido do zero no [Curso de PHP 7](https://www.udemy.com/curso-completo-de-php-7/) disponível na plataforma da Udemy e no site do [HTML5dev.com.br](https://www.html5dev.com.br/curso/curso-completo-de-php-7).
+Projeto desenvolvido do zero no [Curso de PHP 7](https://www.udemy.com/curso-completo-de-php-7/) 
+Feito nas orientações do curso. A ideia é que ao final eu melhore ele usando frameworks e métodos mais atuais, já que o curso é de certa forma datado.
 
 Template usado no projeto [Almsaeed Studio](https://almsaeedstudio.com)
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitba22984edd5e3398bc2082f3f3d67577
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -42,7 +52,8 @@ class ComposerStaticInitba22984edd5e3398bc2082f3f3d67577
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitba22984edd5e3398bc2082f3f3d67577::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitba22984edd5e3398bc2082f3f3d67577::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitba22984edd5e3398bc2082f3f3d67577::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitba22984edd5e3398bc2082f3f3d67577::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitba22984edd5e3398bc2082f3f3d67577::$classMap;
 
